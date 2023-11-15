538d36c72ac266856f9339ab6e15e10cf2f979e8
= <=>
2023-11-15 22:47:59 +0700

    fix formatted date

--- src/Pages/DetailNotes.jsx
+++ src/Pages/DetailNotes.jsx
@@ -1,14 +1,27 @@
 import { useParams } from "react-router-dom";
 import { useNotesContext } from "../Hooks/DataNotesContext";
-import ErrorPage from "./404";
+import ErrorPage from "./ErrorPage";
 
 const DetailNotes = () => {
   const { items } = useNotesContext();
   const { id } = useParams();
   const item = items.find((p) => p.id === parseInt(id, 10));
+
   if (!item) {
     return <ErrorPage />;
   }
+
+  const showFormattedDate = (date) => {
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    return new Date(date).toLocaleDateString("id-ID", options);
+  };
+
+  const formattedDate = showFormattedDate(item.createdAt);
 
   return (
     <div className="flex items-center justify-center min-h-screen">
@@ -20,7 +33,7 @@ const DetailNotes = () => {
           {item.body}
         </p>
         <p className="mx-4 mt-12 text-base break-words text-end font-Poppins">
-          {item.createdAt}
+          {formattedDate}
         </p>
       </div>
     </div>
